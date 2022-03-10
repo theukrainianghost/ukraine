ac09430ba84d35e20a96471c163aa61d86fc9c5b
ukrainian ghost <theukrainianghost@gmail.com>
2022-03-10 22:07:18 +0100

    jeu. 10 mars 2022 22:07:18 CET

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ukraine
+Web site about russian invasion in ukraine
+
+
+You can copy and hosting this website
+
+You can send the link and I will publish it.
+
+The actual list of mirrors are :
+- https://theukrainianghost.github.io/russianliesaboutukraine/
+- https://www.russianliesaboutukraine.com
+
+
+To publish web site 
+- Requirements : https://jekyllrb.com/docs/installation/
+- update _config.yml with you url
+- to deploy : https://jekyllrb.com/docs/deployment/
+- send me by email : theukrainianghost@gmail.com your url
---
+++ ru/publish.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+rm -rf _config.yml
+cp _config.yml.github _config.yml
+JEKYLL_ENV=production jekyll build
+git add .
+now=$(date)
+git commit -m "$now"
+git push
+cd _site
+rm -rf publish.sh
+touch .nojekyll
+git add .
+now=$(date)
+git commit -m "$now"
+git push --force --set-upstream origin gh-pages
+cd ..
+cd _site_github
+shopt -s extglob
+rm -rf !(.git)
+cd ..
+cp -r ./_site/* ./_site_github
+cd _site_github
+rm -rf publish.sh
+touch .nojekyll
+git add .
+now=$(date)
+git commit -m "$now"
+git push --force --set-upstream origin gh-pages
+cd ..
+rm -rf _config.yml
+cp _config.yml.docker _config.yml
+JEKYLL_ENV=production jekyll build
+sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm -rf /disk/save/containers/ukr/*"
+sshpass -p "ukraine69@" scp -r _site/* ukraine@192.168.0.189:/disk/save/containers/ukr
+sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm /disk/save/containers/ukr/publish.sh"
