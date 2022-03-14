a4ee8e35994d6c845c92cb607cbdaa1537002dab
ukrainian ghost <theukrainianghost@gmail.com>
2022-03-14 14:51:46 +0100

    lun. 14 mars 2022 14:51:46 CET

--- fr/publish.sh
+++ fr/publish.sh
@@ -85,12 +85,12 @@ now=$(date)
 git commit -m "$now"
 git push --force --set-upstream origin gh-pages
 cd ..
-rm -rf _config.yml
-cp _config.yml.docker _config.yml
-JEKYLL_ENV=production jekyll build
-sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm -rf /disk/save/containers/ukr/*"
-sshpass -p "ukraine69@" scp -r _site/* ukraine@192.168.0.189:/disk/save/containers/ukr
-sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm /disk/save/containers/ukr/publish.sh"
+# rm -rf _config.yml
+# cp _config.yml.docker _config.yml
+# JEKYLL_ENV=production jekyll build
+# sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm -rf /disk/save/containers/ukr/*"
+# sshpass -p "ukraine69@" scp -r _site/* ukraine@192.168.0.189:/disk/save/containers/ukr
+# sshpass -p "ukraine69@" ssh ukraine@192.168.0.189 "rm /disk/save/containers/ukr/publish.sh"
 
 repparse=${rep:18}
 repparse=${repparse::-9}
